d06e11a879e02f2a14ddcff59e8e7712f858f509
tomascardoso4633@gmail.com <tomascardoso.dev@gmail.com>
2024-04-26 12:35:09 +0100

    Added ICanFly which gets the number of wings to the Bat and the Bee

--- AnimalKingdom/Bat.cs
+++ AnimalKingdom/Bat.cs
@@ -3,6 +3,7 @@
     public class Bat : Animal
     {
         public int NumberOfNipples { get; } = 2;
+        public int NumberOfWings { get; } = 2;
         public override string Sound()
         {
             return base.Sound() + "Screech!";
--- AnimalKingdom/Bee.cs
+++ AnimalKingdom/Bee.cs
@@ -2,6 +2,7 @@
 {
     public class Bee : Animal
     {
+        public int NumberOfWings { get; } = 2;
         public override string Sound()
         {
             return base.Sound() + "Buzz!";
